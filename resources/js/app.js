71f7fd178b8d2a81f5add8f71e322ea61968f16f
bbbrtk <sobkowiak.bart@gmail.com>
2020-05-14 00:23:09 +0200

    add list with buttons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,23 @@
 "use strict"
 
 
-function fetchGithubApi() {
-    let username = document.getElementById("input-username").value;
-    let url = "https://api.github.com/users/" + username
+function fetchGithubApi(url) {
+    console.log("fetch from: " + url)
     fetch(url)
         .then(response => response.json())
         .then(response => {
-            console.log(response);
-            addToTable(response.login);
+            // check if list -> display different then
+            Object.keys(response).forEach(key => {
+                if (response[key]) {
+                    let value = response[key]
+                    if (value.length > 0) {
+                        if (isValidUrl(value)) addButtonToTable(key, value);
+                        else addRowToTable(key, value);
+                    }
+                }
+            });
+
         })
-        // .then(response => {
-        //     addToTable(response.login)
-        // })
 }
 
 function addEventToInput() {
@@ -20,21 +25,57 @@ function addEventToInput() {
     inputUsername.addEventListener("keyup", (event) => {
         event.preventDefault();
         if (event.keyCode === 13) {
-            fetchGithubApi()
+            let username = document.getElementById("input-username").value;
+            let url = "https://api.github.com/users/" + username
+            fetchGithubApi(url)
         }
     });
 }
 
-function addToTable(content){
-    let table = document.getElementById("user-table")
-    let row = table.insertRow()
-    let cell = row.insertCell(0)
-    cell.innerHTML = content
-    row.insertCell(0)
-    console.log(row)
+function addRowToTable(name, content) {
+    let row = document.getElementById("user-table-body").insertRow()
+    row.insertCell().innerHTML = name
+    row.insertCell().innerHTML = content
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+function addButtonToTable(name, content) {
+    let row = document.getElementById("user-table-body").insertRow()
+    row.insertCell().innerHTML = name
+
+    let btn = document.createElement("BUTTON");
+    btn.classList.add("gt-link-button")
+    btn.innerHTML = "check";
+    btn.value = content
+    addButtonListener(btn)
+    row.insertCell().appendChild(btn);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
     addEventToInput()
+
 });
 
+function addButtonListener(button) {
+    button.addEventListener('click', () => {
+        console.log(button.value)
+        if (button.value.includes("https://api.github.com/")) {
+            // clear table
+            let old_tbody = document.getElementById("user-table-body")
+            let new_tbody = document.createElement('tbody');
+            new_tbody.id = "user-table-body"
+            old_tbody.parentNode.replaceChild(new_tbody, old_tbody)
+            // fetch
+            fetchGithubApi(button.value)
+        }
+    });
+}
+
+
+function isValidUrl(string) {
+    try {
+        new URL(string);
+    } catch (_) {
+        return false;
+    }
+    return true;
+}
