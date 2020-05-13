cfd930c6f9c54e74cfabf35db1a8a4e1e7f13c90
bbbrtk <sobkowiak.bart@gmail.com>
2020-05-13 15:39:05 +0200

    add table

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,23 +1,18 @@
 "use strict"
 
-function showResponse(response) {
-    let theDiv = document.getElementById("gt-response-text");
-    let content = document.createTextNode(response);
-    theDiv.appendChild(content);
-}
 
-function getInputValue() {
-    return document.getElementById("input-username").value;
-};
-
-function fetchGithubApi(username) {
+function fetchGithubApi() {
+    let username = document.getElementById("input-username").value;
     let url = "https://api.github.com/users/" + username
     fetch(url)
         .then(response => response.json())
         .then(response => {
             console.log(response);
-            // showResponse(response);
+            addToTable(response.login);
         })
+        // .then(response => {
+        //     addToTable(response.login)
+        // })
 }
 
 function addEventToInput() {
@@ -25,10 +20,18 @@ function addEventToInput() {
     inputUsername.addEventListener("keyup", (event) => {
         event.preventDefault();
         if (event.keyCode === 13) {
-            let username = getInputValue()
-            fetchGithubApi(username)
+            fetchGithubApi()
         }
     });
+}
+
+function addToTable(content){
+    let table = document.getElementById("user-table")
+    let row = table.insertRow()
+    let cell = row.insertCell(0)
+    cell.innerHTML = content
+    row.insertCell(0)
+    console.log(row)
 }
 
 document.addEventListener("DOMContentLoaded", function () {
